567817367774612fc030de4818b6ded1e9dbeec0
Gabriel Borges <igblborges@gmail.com>
2021-10-25 16:34:47 -0300

    requisito-3-solucionando-font-awsome

--- src/components/Store.jsx
+++ src/components/Store.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default class Store extends React.Component {
   render() {
@@ -7,6 +8,7 @@ export default class Store extends React.Component {
         <h1 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h1>
+        <button data-testid="shopping-cart-button">Carrinho</button>
       </div>
     );
   }

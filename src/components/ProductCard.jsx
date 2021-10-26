8ab3d82afeccf533cc9fe9d650f8033d33738c7b
Joao Marco <joaomarco.matta@gmail.com>
2021-10-26 00:21:27 -0300

    Corrige PropTypes e deixa cards responsivos.

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import PropTypes from 'prop-types';
+import styles from '../styles.module.css';
 
 function ProductCard({ title, thumbnail, price }) {
   return (
-    <Card style={ { width: '10rem' } } data-testid="product">
+    <Card style={ { width: '12rem' } } data-testid="product" className="h-100">
       <Card.Img variant="top" src={ thumbnail } />
       <Card.Body>
-        <Card.Text>
-          { title }
-        </Card.Text>
+        <div style={ { height: '5rem' } }>
+          <Card.Text
+            className={ styles.productTitle }
+          >
+            { title }
+          </Card.Text>
+        </div>
         <Card.Title>{ price }</Card.Title>
         <Button variant="primary">Adicionar ao carrinho</Button>
       </Card.Body>
@@ -18,6 +24,9 @@ function ProductCard({ title, thumbnail, price }) {
 }
 
 ProductCard.propTypes = {
-}.isRequired;
+  title: PropTypes.string.isRequired,
+  thumbnail: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+};
 
 export default ProductCard;
